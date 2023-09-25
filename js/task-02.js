2d2b86f628ca981c0f28f1591f4aa441a21588c3
Kostyantin-spec <134406433+Kostyantin-spec@users.noreply.github.com>
2023-09-25 07:17:13 +0300

    2\0

--- js/task-02.js
+++ js/task-02.js
@@ -6,11 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const listEl = document.querySelector('#ingredients');
+// const listEl = document.querySelector('#ingredients');
 
-ingredients.forEach((el) => {
-  const liEl = document.createElement('li');
-  liEl.classList.add('item');
-  liEl.textContent = `${el}`;
-  listEl.insertAdjacentElement('beforeend', liEl)
-});
+// ingredients.forEach((el) => {
+//   const liEl = document.createElement('li');
+//   liEl.classList.add('item');
+//   liEl.textContent = `${el}`;
+//   listEl.insertAdjacentElement('beforeend', liEl)
+// });
+
+
+const listEl = document.querySelector("#ingredients");
+
+const markup = ingredients
+  .map(el => {
+    const liEl = document.createElement('li');
+    liEl.classList.add('item');
+    liEl.textContent = `${el}`;
+    return liEl.outerHTML;
+  }).join("");
+
+listEl.insertAdjacentHTML("beforeend", markup);
